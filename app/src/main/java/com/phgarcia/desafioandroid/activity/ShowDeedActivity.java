1ad46b656b3b3eda9a4d7e4dfe442b2f091df521
Pedro Garcia <garcia.ph@gmail.com>
2017-12-20 03:21:35 -0200

    ImageView set to show image from external storage + layout changes

--- app/src/main/java/com/phgarcia/desafioandroid/activity/ShowDeedActivity.java
+++ app/src/main/java/com/phgarcia/desafioandroid/activity/ShowDeedActivity.java
@@ -1,8 +1,11 @@
 package com.phgarcia.desafioandroid.activity;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.phgarcia.desafioandroid.R;
 import com.phgarcia.desafioandroid.helper.ShowDeedHelper;
@@ -17,11 +20,26 @@ public class ShowDeedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_deed);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         Intent intent = getIntent();
         this.deed = (Deed) intent.getSerializableExtra("deed");
 
         helper = new ShowDeedHelper(this, deed);
-        helper.fillInfo();
+        helper.fillInfo(this);
+
+        findViewById(R.id.deed_website_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String uriString = deed.getSite();
+                if (!uriString.startsWith("http://")) {
+                    uriString = "http://" + uriString;
+                }
+
+                Intent intentVisitWebsite = new Intent(Intent.ACTION_VIEW);
+                intentVisitWebsite.setData(Uri.parse(uriString));
+                startActivity(intentVisitWebsite);
+            }
+        });
     }
 }
--- app/src/main/java/com/phgarcia/desafioandroid/helper/ShowDeedHelper.java
+++ app/src/main/java/com/phgarcia/desafioandroid/helper/ShowDeedHelper.java
@@ -1,32 +1,55 @@
 package com.phgarcia.desafioandroid.helper;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.media.MediaScannerConnection;
+import android.media.audiofx.EnvironmentalReverb;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.phgarcia.desafioandroid.R;
 import com.phgarcia.desafioandroid.activity.ShowDeedActivity;
 import com.phgarcia.desafioandroid.model.Deed;
+import com.phgarcia.desafioandroid.util.Constants;
+
+import java.io.File;
 
 /**
  * Created by garci on 12/20/2017.
  */
 
 public class ShowDeedHelper {
-    private final TextView nameField;
-    private final TextView websiteField;
-    private final TextView descriptionField;
+    private final TextView  nameField;
+    private final TextView  descriptionField;
+    private final ImageView imageField;
 
     private Deed deed;
 
     public ShowDeedHelper (ShowDeedActivity activity, Deed deed) {
-        this.nameField        = (TextView) activity.findViewById(R.id.deed_name);
-        this.websiteField     = (TextView) activity.findViewById(R.id.deed_site);
-        this.descriptionField = (TextView) activity.findViewById(R.id.deed_description);
+        this.nameField        = (TextView)  activity.findViewById(R.id.deed_name);
+        this.descriptionField = (TextView)  activity.findViewById(R.id.deed_description);
+        this.imageField       = (ImageView) activity.findViewById(R.id.deed_image);
         this.deed = deed;
     }
 
-    public void fillInfo() {
+    public void fillInfo(Context context) {
         this.nameField.setText(this.deed.getName());
-        this.websiteField.setText(this.deed.getName());
         this.descriptionField.setText(this.deed.getDescription());
+
+        File image = new File(deed.getImagePath());
+
+        if (image.exists()) {
+            Bitmap bmp = BitmapFactory.decodeFile(image.getAbsolutePath());
+            imageField.setImageBitmap(bmp);
+        } else {
+            Log.d("Image", "Image file doesn't exist");
+        }
+
     }
 }
