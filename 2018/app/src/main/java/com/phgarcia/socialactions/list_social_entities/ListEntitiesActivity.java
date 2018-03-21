ad20d43e6b9833051a77e3d16a9b7deea1d5b580
Pedro Garcia <garcia.ph@gmail.com>
2018-03-20 23:12:15 -0300

    Button to access website added

--- 2018/app/src/main/java/com/phgarcia/socialactions/list_social_entities/ListEntitiesActivity.java
+++ 2018/app/src/main/java/com/phgarcia/socialactions/list_social_entities/ListEntitiesActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
--- 2018/app/src/main/java/com/phgarcia/socialactions/social_entity_details/SocialEntityDetailsActivity.java
+++ 2018/app/src/main/java/com/phgarcia/socialactions/social_entity_details/SocialEntityDetailsActivity.java
@@ -1,7 +1,10 @@
 package com.phgarcia.socialactions.social_entity_details;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -10,12 +13,13 @@ import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SocialEntityDetailsActivity extends AppCompatActivity implements SocialEntityDetailsView {
 
-    @BindView(R.id.tv_entity_details_name) TextView tv_entity_name;
-    @BindView(R.id.tv_entity_details_description) TextView tv_entity_description;
-    @BindView(R.id.iv_entity_details_header) ImageView iv_entity_cover_image;
+    @BindView(R.id.tv_entity_details_name) TextView tvEntityName;
+    @BindView(R.id.tv_entity_details_description) TextView tvEntityDescription;
+    @BindView(R.id.iv_entity_details_header) ImageView ivEntityCoverImage;
 
     SocialEntityDetailsPresenter presenter;
 
@@ -35,18 +39,34 @@ public class SocialEntityDetailsActivity extends AppCompatActivity implements So
     }
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
     public void showDetails() {
         String entityName = getIntent().getStringExtra("ENTITY_NAME");
         String entityDescription = getIntent().getStringExtra("ENTITY_DESCRIPTION");
-        String entityWebsite = getIntent().getStringExtra("ENTITY_WEBSITE");
         String entityCoverImageUrl = getIntent().getStringExtra("ENTITY_COVER_IMAGE");
 
         Picasso.with(this)
                 .load(entityCoverImageUrl)
                 .centerCrop()
                 .fit()
-                .into(iv_entity_cover_image);
-        tv_entity_name.setText(entityName);
-        tv_entity_description.setText(entityDescription);
+                .into(ivEntityCoverImage);
+        tvEntityName.setText(entityName);
+        tvEntityDescription.setText(entityDescription);
+    }
+
+    @OnClick(R.id.fab_go_to_website)
+    public void openBrowser(){
+        Intent openUrlInBrowser = new Intent(Intent.ACTION_VIEW, Uri.parse(getIntent().getStringExtra("ENTITY_WEBSITE")));
+        startActivity(openUrlInBrowser);
     }
 }
